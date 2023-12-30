92abb90cb3227cb15937b43053dd57a1e92187bd
lmessmer <134856540+lmessmer@users.noreply.github.com>
2023-12-30 10:52:59 -0600

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,41 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_new_1 = new ol.layer.Tile({
+            'title': 'new',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'file:///C:/SpatialData/Map Tiles/FortTilesTest/xzt/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_tiletest_2 = new ol.layer.Tile({
+            'title': 'tiletest',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://lmessmer.github.io/TestTiles/{Z}/{X}/{Y}.png'
+            })
+        });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_new_1.setVisible(true);lyr_tiletest_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_new_1,lyr_tiletest_2];
